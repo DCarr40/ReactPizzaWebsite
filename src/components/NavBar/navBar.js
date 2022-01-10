a92eca0172e3f17f3f64f40a007e996f7f230b70
DCarr40 <davidwcarruthers@yahoo.com>
2022-01-09 21:35:02 -0500

    added an h1 tag in Navbar component

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const navBar = () => {
-  return <div></div>;
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
 };
 
-export default navBar;
+export default Navbar;
