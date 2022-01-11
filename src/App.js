de8cac81fb80e69bc298f9d66f9dc61c0105904f
DCarr40 <davidwcarruthers@yahoo.com>
2022-01-10 19:47:52 -0500

    added kanit reg and bold font family in index html and in global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./components/NavBar/index.js";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalStyle } from "./GlobalStyles.js";
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
     </Router>
   );
